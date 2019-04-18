41cbb0c13cd9fe7e71b87ecc05d2f1b5d1956be6
Isaure Blaffa <isaureblaffa.pro@gmail.com>
2019-04-18 19:34:19 +0200

    add function and link to api for having a aleatoire egg (in franglish, please)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Router from '../src/components/router/Router';
+import AleatoirEgg from '../src/components/aleatoirEgg/AleatoirEgg'
 import './App.css';
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Router />
+        <AleatoirEgg />
       </React.Fragment>
     );
   }
--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../homePage/HomePage';
+import AleatoirEgg from '../aleatoirEgg/AleatoirEgg'
+
 
 export default function Router() {
   return (
     <Switch>
       <Route path='/' component={HomePage} />
+      <Route path='/aleatoirEgg' component={AleatoirEgg} />
     </Switch>
   );
 }
